776da81f9da5b3b1f1131d4cd22a61d4c74204b3
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-22 11:03:22 +0000

    Added connection to MongoDB database

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
+
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  // MongoDB database connection string
+  const uri =
+    "mongodb+srv://" +
+    process.env.DB_USER +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.CLUSTER +
+    "/" +
+    process.env.DB +
+    "?retryWrites=true&w=majority&appName=" +
+    process.env.APP_NAME;
 
+  // Connect to the MongoDB database
+  mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB connection error:", err));
+
+  app.route("/api/threads/:board");
+
+  app.route("/api/replies/:board");
 };
